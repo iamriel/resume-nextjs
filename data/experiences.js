59c9d9c68f3c190a01adc2b49be8daf5efcf0c70
Rieljun Liguid <me@iamriel.com>
2020-02-28 08:27:51 +0800

    Add more experiences

--- data/experiences.js
+++ data/experiences.js
@@ -4,13 +4,19 @@ const experiences = [
     company: {
       name: 'Upwork',
       location: 'Remote',
-      description: 'Employee management system',
+      description: 'Worked on multiple clients and projects here.',
     },
     period: {
       start: '07/2018',
       end: 'present',
     },
-    tasks: ['Build RESTful API with Django Rest Framework'],
+    tasks: [
+      'Rocketmail (rocketmail.lu) -- Build RESTful API with Django Rest Framework and Server Side Rendered frontend site with Vue/Nuxt. Created backend that manages contacts and sending campaign.  In the frontend, I built a drag and drop email template builder that will be used in sending of dynamic user emails.',
+      'ILS -- Used NodeJS and Express for building RESTful api and React for building frontend side. Built a calendar and scheduling system which automatically sends notifications realtime with the help of websockets. Implement Organization management which contains members and different groups. Handle necessary security implementation both in the backend and frontend.',
+      'T-mis (t-mis.fr) -- Built a django powered website that enables users to upload a scanned copy of an invoice (pdf) and copy of products received and then the system automatically identifies anomalies based on the invoice and the list of products and quantities received. Added VueJS in the frontend together with django template to add fluidity of UI/UX.',
+      'Hikster (hikster.com) -- Implemented a website that shows hiking places with the map and trail data, i.e. difficulty, elevation. Implemented a feature that enables admins to draw trails in a map and automatically compute necessary information (difficulty, elevation).  Used Django REST Framework for building API with geodjango and postgres postgis for processing geospatial information.',
+      'Metronom -- Used DRF for building RESTful API and React on the frontend. The system enables organization to create project and assign users for the tasks in completing the project.  The user can track their progress, time, holidays and budget.',
+    ],
   },
   {
     position: 'Django / React Developer / DevOps',
@@ -70,7 +76,7 @@ const experiences = [
       'Develop online course platform for Gurulabs (gurulabs.com). Design database structure and implement dynamic form builder for creating dynamic data based on a course curriculum.',
       'Implement business processes with python for Advisordeck (wealthvest.com). Manage different types of users and permissions.  Implement charts that shows the financial status and updates.',
     ],
-    outcome: 'Found better opportunity with another company.'
+    outcome: 'Found better opportunity with another company.',
   },
   {
     position: 'PHP Developer',
@@ -87,7 +93,7 @@ const experiences = [
       'Create website that manages schedules of movies to be shown in the cinemas.',
       'Implement comment and ratings system by the users.',
     ],
-    outcome: 'Project finished successfully.'
+    outcome: 'Project finished successfully.',
   },
 ];
 
